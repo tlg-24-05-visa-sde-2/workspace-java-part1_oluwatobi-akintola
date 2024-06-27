606a7351eed3a53966c4190828e03563802275ec
Oluwatobi Akintola <oluwatobia@tlgcohort.com>
2024-06-27 10:46:31 -0500

    initial commit

---
+++ Lab01.1/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Classic Hello World application in java
+ * This is a standalone application (does not run on an app server)
+ */
+class HelloWorld {
+
+    //Starting point for every standalone java app
+    public static void main(String[] args) {
+        System.out.println("Hello From IntelliJ"); //Prints to the console
+        System.out.println("This is nicer than scratch programming");
+    }
+}
